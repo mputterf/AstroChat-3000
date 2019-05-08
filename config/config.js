184f88f3922673f3b78d4ee2438c2b30896e3f91
Michael Putterflam <michael.putterflam@gmail.com>
2019-05-07 21:27:55 -0700

    created database and made it so database passwords are stored in .env

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
-{
+require("dotenv").config();
+module.exports = {
+
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "exampledb",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "chat_db",
     "host": "localhost",
     "dialect": "mysql"
   },
@@ -18,4 +20,6 @@
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
-}
+
+};
+
---
+++ schema.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE chat_db;
+
+USE chat_db;
