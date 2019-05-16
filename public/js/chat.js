4cd406736ebae4ab57b88a77623760443862c40d
Michael Putterflam <michael.putterflam@gmail.com>
2019-05-15 21:53:55 -0700

    User messages send and recieve from client and server. Database post route created and working, commented out until sign-in works

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,31 @@
 $(function () {
+    // var userID = 1;
+
     var socket = io();
-    // Get the message the user entered and send it to the server
+    // Get the message the user entered and send it to the server and databse
     $('form').submit(function (e) {
         e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#message-box').val());
+        var message = $('#message-box').val();
+
+        socket.emit('chat message', message);
+
+
+        // console.log("Message: ", message);
+
+        // Create message object for sending to database
+        // var userPost = {
+        //     textInput: message,
+        //     UserId: userID
+        // };
+
+        // post route for sending message to database
+        // $.post("/api/posts", userPost, function () {
+        //     console.log("Message sent to db");
+        // });
+
+        // clear message box after we send everything
         $('#message-box').val('');
+
         return false;
     });
 
